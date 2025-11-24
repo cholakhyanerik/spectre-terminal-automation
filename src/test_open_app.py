ca95aa92f4b790d835e5d89857cefb698d637fe1
Erik Cholakhyan <cholakhyanerik@gmail.com>
2025-11-24 14:08:42 +0400

    Fixed first test

--- src/test_open_app.py
+++ src/test_open_app.py
@@ -31,11 +31,19 @@ def test_open_maximize_and_add_orderbook_withCoin(app):
     # Optional: wait a bit to observe the click effect
     time.sleep(5)
 
+    ################################################################
+    centreAddOrderBookModalCoin = main_window.rectangle()
+    x = centreAddOrderBookModalCoin.left + 942   # 50 pixels from left edge (adjust if needed)
+    y = centreAddOrderBookModalCoin.top + 552    # 25 pixels from top edge (adjust if needed)
+    time.sleep(2)
+    mouse.click(coords=(x, y))
     mouse.click(coords=(x, y))
     time.sleep(5)
     # Assert window is still visible after click
     assert main_window.is_visible(), "Main window is not visible"
 
+
+    ######################################################################
 
     # Now click '+ Order Book' near the top-left corner
     addDefauledCoinWIchSelected = main_window.rectangle()

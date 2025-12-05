6df5752e6e22e4d3304ba4cab1c7e7d3fc1c7b02
Erik Cholakhyan <cholakhyanerik@gmail.com>
2025-12-05 13:51:06 +0400

    Fix

--- src/test_open_meta_app_all_flow_test.py
+++ src/test_open_meta_app_all_flow_test.py
@@ -1,17 +1,20 @@
 # src\test_open_meta_app_all_flow_test.py
 
 import time
+from pywinauto import mouse
+from pywinauto.keyboard import send_keys
 
 def test_only_meta_app(meta_app):
     """This test launches ONLY the MetaScalp application."""
     
     # Get main window
-    main_window = meta_app.window(title_re=".*")
+    main_window = meta_app.window()
+    time.sleep(10)
+    send_keys("{VK_LWIN down}{TAB}{VK_LWIN up}")
+
+    time.sleep(4)
     main_window.wait("visible", timeout=15)
 
-    # Maximize
-    main_window.maximize()
-    time.sleep(1)
 
     time.sleep(30)
     assert main_window.is_visible(), "MetaScalp window did not open properly"

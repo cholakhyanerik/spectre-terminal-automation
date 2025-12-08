903b163b40a6959638bb7de6302487a041bf9149
Erik Cholakhyan <cholakhyanerik@gmail.com>
2025-12-08 14:07:56 +0400

    Checkers Meta Scalp app

--- src/test_open_meta_app_all_flow_test.py
+++ src/test_open_meta_app_all_flow_test.py
@@ -7,14 +7,6 @@ from pywinauto.keyboard import send_keys
 def test_only_meta_app(meta_app):
     """This test launches ONLY the MetaScalp application."""
     
-    # Get main window
-    main_window = meta_app.window()
-    time.sleep(10)
-    send_keys("{VK_LWIN down}{TAB}{VK_LWIN up}")
-
-    time.sleep(4)
-    main_window.wait("visible", timeout=15)
 
 
-    time.sleep(30)
-    assert main_window.is_visible(), "MetaScalp window did not open properly"
+    time.sleep(20)

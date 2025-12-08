fc20e3b3371a65474af94d66cabbada37258383f
Erik Cholakhyan <cholakhyanerik@gmail.com>
2025-12-08 14:26:05 +0400

    Fix Meta App

--- src/test_open_meta_app_all_flow_test.py
+++ src/test_open_meta_app_all_flow_test.py
@@ -1,12 +1,23 @@
-# src\test_open_meta_app_all_flow_test.py
-
+from pywinauto import Desktop
 import time
-from pywinauto import mouse
-from pywinauto.keyboard import send_keys
 
 def test_only_meta_app(meta_app):
-    """This test launches ONLY the MetaScalp application."""
-    
+    print("Ждём появления окна MetaScalp...")
 
+    timeout = 25
+    for _ in range(timeout):
+        windows = Desktop(backend="uia").windows()
+        for w in windows:
+            if "MetaScalp" in w.window_text():
+                main_window = w
+                print("Нашли окно MetaScalp!")
+                
+                # Проверяем, не свернуто ли окно, и восстанавливаем если нужно
+                if main_window.is_minimized():
+                    main_window.restore()
 
-    time.sleep(20)
+                assert main_window.is_visible() and not main_window.is_minimized(), "Окно приложения недоступно!"
+                return
+        time.sleep(1)
+
+    raise RuntimeError("Окно MetaScalp не появилось за 25 секунд")
